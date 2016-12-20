5fed7bbce00d777819a92a99dd0bc49eeeec798a
trnoam <noam10trobishi@gmail.com>
2016-12-20 14:22:05 +0200

    erased wowoowowow variable

--- app/src/main/java/com/scool/scool/OpenActivity.java
+++ app/src/main/java/com/scool/scool/OpenActivity.java
@@ -1,5 +1,6 @@
 package com.scool.scool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class OpenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
